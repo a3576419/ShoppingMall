ab831e2d1cbd8901cc87f6827f0cd68807113fde
a3576419@163.com <a3576419@163.com>
2020-09-12 20:40:41 +0800

    请求轮播图数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+<!--  首页-->
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "@/components/common/navbar/NavBar";
+  import {getHomeMultidata} from "@/network/home";
   export default {
-    name: "Home"
+    name: "Home",
+    components:{
+      NavBar
+    },
+    data(){
+      return{
+        // result:null
+        banners:[],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res=>{
+        console.log(res);
+        // this.result=res;
+        this.banners=res.data.data.banner.list;
+        this.recommends=res.data.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    /*使用变量设置颜色*/
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
